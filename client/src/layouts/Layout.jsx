1c38943ada518b214eab464543bf66f0af5330f4
Spencer Peterson <spencerpeterson@MacBook-Pro-4.lan>
2021-07-23 12:39:56 -0400

    add layout

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Nav from './Nav';
+import Footer from './Footer';
 
 export default function Layout(props) {
-  return <div>{props.children}</div>;
+  return (
+    <div>
+      <Nav />
+      {props.children}
+      <Footer />
+    </div>
+  );
 }
